ad3a27c377d331f7db7555788c9024a50531c473
bRad <brad@humanenginuity.com>
2017-08-10 17:36:54 -0700

    Renamed project to fluent_validator

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@
 #![warn(missing_debug_implementations, trivial_casts, trivial_numeric_casts, unused_import_braces, unused_qualifications)]
 #![deny(unused_must_use, overflowing_literals)]
 
-extern crate RustQuickstartTemplate;
-use RustQuickstartTemplate::Args;
+extern crate fluent_validator;
+use fluent_validator::Args;
 
 pub fn main() {
-    match RustQuickstartTemplate::run(Args::from(std::env::args_os())) {
+    match fluent_validator::run(Args::from(std::env::args_os())) {
         Ok(_) => (),
         Err(e) => println!("{}", e),
     }
