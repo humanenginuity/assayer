2de082913e3a571d68caae8d691cd9fc5337131c
bRad <brad@humanenginuity.com>
2017-08-11 20:07:54 -0700

    added copyright headers, notices and licensing information

--- src/consts/msgs/en_us.rs
+++ src/consts/msgs/en_us.rs
@@ -1,3 +1,12 @@
+// Copyright 2017 Human Enginuity. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //error.rs
 pub const MSG_ERR_VALIDATION_VALUE_EMPTY: &'static str   = "Empty Value Error";
 pub const MSG_ERR_VALIDATION_VALUE_INVALID: &'static str = "Invalid Value Error";
